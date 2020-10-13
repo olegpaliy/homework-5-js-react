15173ae4a8921a5fae95ff3b5e9cd4643b2f819b
Oleh <goleh@ukr.net>
2020-10-13 15:17:12 +0300

    add 2 tasks

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//завдання 1
+let cube = (a) => a * a * a;
+console.log(cube(5));
+
+//завдання 2
+let numb = (a,b,c) => (a + b) / c;
+console.log(numb(5, 7, 10));
+
+//завдання 3 
+let arrCount = [1,2,3,4,5,6,7];
